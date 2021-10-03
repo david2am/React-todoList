a8eec769159522437a8cefeccc8cde0234e1af79
David De Vargas <dvccueter@gmail.com>
2021-10-03 08:45:03 -0500

    fix: localstorage functionality fixed

--- src/components/organisms/Main.tsx
+++ src/components/organisms/Main.tsx
@@ -1,28 +1,32 @@
-import { FormEvent, useEffect, useRef, useState } from "react"
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
 
 const Main = (): JSX.Element => {
+  const [value, setValue] = useState<string>('')
   const [todoList, setTodoList] = useState<string[]>([])
 
-  const searchValue = useRef<HTMLInputElement>(null)
-
   useEffect(() => {
-    const existingTodoList = localStorage.getItem('todoList')
-    existingTodoList && setTodoList(JSON.parse(existingTodoList) as string[])
+    const existingTodoList = localStorage.getItem('todoList')    
+    existingTodoList && setTodoList(existingTodoList.split(','))
   }, [])
 
   const handleSubmit = (e: FormEvent): void => {
     e.preventDefault()
-    if (searchValue.current) {
-      const newTodoList = [...todoList, searchValue.current.value]
-      localStorage.setItem('todoList', newTodoList.toString())
-      setTodoList(newTodoList)
-    }
+    addTodo()
+    setValue('')
+  }
+
+  const handleValue = (e: ChangeEvent<HTMLInputElement>): void => setValue(e.target.value)
+
+  function addTodo() {
+    const newTodoList = [...todoList, value]
+    localStorage.setItem('todoList', newTodoList.toString())
+    setTodoList(newTodoList)
   }
   
   return (
     <main>
       <form onSubmit={handleSubmit}>
-        <input type="text" ref={searchValue} />
+        <input type="text" value={value} onChange={handleValue} />
         <button>Add new one</button>
       </form>
       <ul>
